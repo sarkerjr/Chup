34c2a126598add0dbce9d775fec38efa5aecffb3
Anik Sarker <sarkerjr@gmail.com>
2023-09-18 00:08:16 +0600

    refactor - margin added to body

--- client/src/routes/UserRoutes.tsx
+++ client/src/routes/UserRoutes.tsx
@@ -1,12 +1,9 @@
 import { lazy } from 'react';
 
-// import GuestLayout from '@/layouts/GuestLayout';
-
 const Chats = lazy(() => import('@/pages/chats'));
 
 const UserRoutes = {
   path: '/',
-  // element: <GuestLayout />,
   children: [
     {
       path: '/',
