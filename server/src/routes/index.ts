71a7e486e925e99b7e9983c81266c45428ea9b06
Anik Sarker <sarkerjr@gmail.com>
2023-11-02 23:38:42 +0600

    feat - conversation api endpoints added

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 
 import userRoutes from './user.routes';
 import profileRoutes from './profile.routes';
+import messageRoutes from './message.routes';
+import conversationRoutes from './conversation.routes';
+import memberRoutes from './member.routes';
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/profile', profileRoutes);
+router.use('/message', messageRoutes);
+router.use('/conversation', conversationRoutes);
+router.use('/member', memberRoutes);
 
 export default router;
