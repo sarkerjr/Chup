154373dbedd6570037fc32df9fdcd1c9bf3bc963
Anik Sarker <sarkerjr@gmail.com>
2023-11-25 14:12:18 +0600

    chore - axios added

---
+++ client/src/utils/axios.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const axiosServices = axios.create({
+  // separate for development and production environments
+  baseURL: process.env.VITE_BASE_URL,
+});
+
+//Intercept all the requests to enter updated token
+axiosServices.interceptors.request.use(
+  (config) => {
+    if (localStorage.getItem('serviceToken')) {
+      config.headers.Authorization = localStorage.getItem('serviceToken');
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//Intercept all the responses to handle errors
+axiosServices.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosServices;
