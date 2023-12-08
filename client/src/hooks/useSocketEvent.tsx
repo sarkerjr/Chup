e70ec1e1a8f9c11e4a2ce4ec21e468019179775e
Anik Sarker <sarkerjr@gmail.com>
2023-12-08 13:55:20 +0600

    refactor - useSocketEvent modified to handle emit events

--- client/src/hooks/useSocketEvent.tsx
+++ client/src/hooks/useSocketEvent.tsx
@@ -1,16 +1,29 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useCallback } from 'react';
 import { SocketContext } from '@/contexts/SocketContext';
 
 export const useSocketEvent = (
   eventName: string,
   handler: (...args: any[]) => void
-): void => {
+) => {
   const socket = useContext(SocketContext);
 
   useEffect(() => {
+    // Register the event listener for receiving events
     socket.on(eventName, handler);
+
+    // Cleanup the event listener when the component unmounts
     return () => {
       socket.off(eventName, handler);
     };
   }, [eventName, handler, socket]);
+
+  // Provide a function to emit events
+  const emitEvent = useCallback(
+    (...args: any) => {
+      socket.emit(eventName, ...args);
+    },
+    [eventName, socket]
+  );
+
+  return emitEvent;
 };
