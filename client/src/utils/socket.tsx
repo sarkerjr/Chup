477e48c4a0ce583c9f0ce49ad2128152197c4be3
Anik Sarker <sarkerjr@gmail.com>
2023-12-09 14:13:01 +0600

    refactor - client socket auth added

--- client/src/utils/socket.tsx
+++ client/src/utils/socket.tsx
@@ -2,6 +2,9 @@ import { io, Socket } from 'socket.io-client';
 
 const socket: Socket = io(import.meta.env.VITE_BASE_URL, {
   transports: ['websocket'],
+  auth: {
+    token: localStorage?.getItem('accessToken'),
+  },
 });
 
 export default socket;
